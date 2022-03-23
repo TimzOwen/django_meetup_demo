f0433e5aeb77640ba33b5e4f15df9c00c0b341b9
TimzOwen <ktimothy@kabarak.ac.ke>
2022-03-23 15:11:31 +0300

    updated css meetup

--- meetups/views.py
+++ meetups/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     meetups = [
-        {'title': 'Meet up 1'},
-        {'title': 'meetup 2'},
-        {'title': 'meet up 3'}
+        {'title': 'Meet up 1', 'venue': 'ihub', 'slug': 'a-first-meetup'},
+        {'title': 'meetup 2', 'venue': 'kabarak', 'slug': 'a-second-meetup'},
+        {'title': 'meet up 3', 'venue': 'Juja', 'slug': 'a-third-meetup'}
     ]
     return render(request, 'meetups/index.html',
                   {'show_meetups': True,
